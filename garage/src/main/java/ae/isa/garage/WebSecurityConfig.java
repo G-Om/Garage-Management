54b35ffbea9caead1552623b922897d9caede80e
Om G <ogavade.isa.ae>
2024-03-15 00:03:03 +0530

    Impl Spring-Security-6 using JWT (with circular references enabled) and InMemoryUserInfo

--- garage/src/main/java/ae/isa/garage/WebSecurityConfig.java
+++ garage/src/main/java/ae/isa/garage/WebSecurityConfig.java
@@ -1,14 +1,26 @@
 package ae.isa.garage;
 
+import ae.isa.garage.filters.JwtRequestFilter;
+import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +29,8 @@ import java.util.List;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     // Registering a bean for User Info Service
     @Bean
@@ -24,16 +38,16 @@ public class WebSecurityConfig {
         List<UserDetails> userDetails = new ArrayList<>();
 
         userDetails.add(
-        User.withDefaultPasswordEncoder()
-                .username("om")
-                .password("root")
+        User
+                .withUsername("om")
+                .password(passwordEncoder().encode("root"))
                 .roles("USER")
                 .build());
 
         userDetails.add(
-        User.withDefaultPasswordEncoder()
-                .username("root")
-                .password("root")
+        User
+                .withUsername("root")
+                .password(passwordEncoder().encode("root"))
                 .roles("ADMIN")
                 .build());
 
@@ -44,18 +58,48 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(
+                        csrf -> csrf.disable()
+                )
                 .authorizeHttpRequests(
                         request -> request
-                                .requestMatchers("/hello").permitAll()
+                                .requestMatchers("/authenticate").permitAll()
                                 .requestMatchers("/*/hello").hasAnyRole("USER", "ADMIN")
                                 .requestMatchers("/admin").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-
+                .authenticationProvider(authenticationProvider())
+                .sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                ).addFilterBefore(
+                        jwtRequestFilter, UsernamePasswordAuthenticationFilter.class
+                );
         return http.build();
     }
 
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+
+    }
+
+
+    // defining a bean so that it can be Autowired
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    // Password Hashing
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
---
+++ garage/src/main/java/ae/isa/garage/util/JwtUtil.java
@@ -0,0 +1,67 @@
+package ae.isa.garage.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtUtil {
+
+
+    private final String Secret_Key = "bb726b76b53500d1ba29173bc89e7fe2dbaeab1dce4c0518fd1190c4de512e08fb7cbba1ec79242671e1e1e7309bf41d8c97b79706974071e1df359f16a539c9";
+
+
+    public String extractUserName(String token) {
+        return  extractClaims(token, Claims::getSubject);
+    }
+    public Date extractExpiration(String token) {
+        return  extractClaims(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extracAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extracAllClaims(String token) {
+        // Changed Syntax
+
+        //return Jwts.parser().setSigningKey(Secret_Key).parseClaimsJws(token).getBody();
+        return Jwts.parser().setSigningKey(Secret_Key).build().parseSignedClaims(token).getPayload();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String generateToken(UserDetails userDetails) {
+//        System.out.println("KEY: " + Secret_Key.getEncoded());
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().setClaims(claims).setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + (1000) * 60 * 60 * 8))
+                .signWith(SignatureAlgorithm.HS256, Secret_Key).compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final  String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+
+
+}
